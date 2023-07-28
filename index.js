cb35c79abe0701919a56b37d4bbbc3b0e1e6fa1d
Moses <moses.oyelade@student.moringaschool.com>
2023-07-28 05:16:36 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector("main");
+// Removing Main#main from DOM
+document.querySelector("main").remove();
+
+// Creating newHeader  variable that points to node 'h1#victory'
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = ('victory');
+
+
+newHeader.textContent = "YOUR-NAME is the champion";
+
